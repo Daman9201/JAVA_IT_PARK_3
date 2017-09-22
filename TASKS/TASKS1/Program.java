942b8b3f946a93cfdaae40f28c2aaaf2aa02e006
unknown <Student17>
2017-09-22 20:13:09 +0300

    refactor first task

--- TASKS/TASKS1/Program.java
+++ TASKS/TASKS1/Program.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 class Program {
 	public static void main(String[] args) {
-		int x = 13;
-		int y = 15;
+		System.out.println("Введите первое значение X");
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+				System.out.println("Введите первое значение Y");
+		int y = scanner.nextInt();
 		int sumResult = x + y;
+
 
 
 		System.out.println(sumResult);
